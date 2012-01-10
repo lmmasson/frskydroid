9f63b66bf36618e91115a9009811660a66d6406c
Laurent MASSON <lmmasson@laposte.net>
2012-01-10 21:47:56 +0100

    Really first step of Bluetooth support

--- src/com/frskydroid/FrskydroidActivity.java
+++ src/com/frskydroid/FrskydroidActivity.java
@@ -3,6 +3,9 @@ package com.frskydroid;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.bluetooth.BluetoothAdapter;
+import android.widget.Toast;
+
 
 public class FrskydroidActivity extends Activity {
     /** Called when the activity is first created. */	          
@@ -11,10 +14,18 @@ public class FrskydroidActivity extends Activity {
     public int tx;
     public int ad1;
     public int ad2;
-	
+    public BluetoothAdapter mBlueToothAdapter = BluetoothAdapter.getDefaultAdapter();
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+
+        if (mBlueToothAdapter==null) {
+        	Toast.makeText(this, "BlueTooth Not supported", Toast.LENGTH_LONG).show();
+        } else {
+        	Toast.makeText(this, "BlueTooth ok", Toast.LENGTH_SHORT).show();	
+        }
     }
 }
